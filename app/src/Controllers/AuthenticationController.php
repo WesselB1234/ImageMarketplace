a183d41fcfe26bfbe2dce842ff04867271844838
WesselB1234 <stosin007@hotmail.com>
2025-11-28 22:20:46 +0100

    More files

---
+++ app/src/Controllers/AuthenticationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\Controller;
+
+class AuthenticationController extends Controller 
+{
+    public function login()
+    {
+        
+    }
+
+    public function logout()
+    {
+        
+    }
+}

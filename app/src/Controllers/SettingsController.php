a3d4df0ee1a23157992a1e78ea8c15d2a3b69807
WesselB1234 <stosin007@hotmail.com>
2026-01-18 12:09:21 +0100

    Account deletion by user self

--- app/src/Controllers/SettingsController.php
+++ app/src/Controllers/SettingsController.php
@@ -4,17 +4,43 @@ namespace App\Controllers;
 
 use App\Controllers\Controller;
 use App\Models\Attributes\Route;
+use App\Services\Interfaces\IUsersService;
+use Exception;
 
 class SettingsController extends Controller
 {
-    public function __construct()
+    private IUsersService $usersService;
+
+    public function __construct(IUsersService $usersService)
     {
         $this->loggedInAuthorization();
+
+        $this->usersService = $usersService;
     }
     
     #[Route("GET", "/settings")]
     public function index()
     {
         $this->displayView();
+    }
+
+    #[Route("GET", "/settings/deleteaccount")]
+    public function deleteAccount()
+    {
+        try{
+            $this->usersService->deleteUserByUserId($_SESSION["user"]->getUserId());
+            
+            unset($_SESSION["user"]);
+            $_SESSION["success_message"] = "Successfully deleted your account.";
+
+            header("location: /login");
+        }
+        catch(Exception $e){
+            $this->displayView([
+                    "errorMessage" => $e->getMessage()
+                ],
+                "Settings/index.php"
+            );
+        } 
     }
 }
--- app/src/Views/Settings/index.php
+++ app/src/Views/Settings/index.php
@@ -8,7 +8,19 @@
 
 <main class="container">
 
-    <h1>Settings</h1>
+    <h1 class="mb-4">Settings</h1>
+
+    <?php 
+        include $partialsDir."/errorAlert.php";
+        include $partialsDir."/successAlert.php"; 
+    ?>
+
+    <h4>Account actions</h4>
+    <ul class="list-unstyled">
+        <li>
+            <a class="btn btn-danger" href="/settings/deleteaccount">Delete account</a>
+        </li>
+    </ul>
 </main>
 
 <?php require $partialsDir."/footer.php"; ?>

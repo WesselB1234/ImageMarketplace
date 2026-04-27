48d81c9f4f756442e609ac9aadb58bfd6411eb0c
WesselB1234 <stosin007@hotmail.com>
2026-04-27 11:44:41 +0200

    Env for baseurl and moving css to proper location

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:80";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:80";
 
 createApp(App)
   .use(router)

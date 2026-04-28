4630c95093172aea573feb5ed887d0e45f3a38be
WesselB1234 <stosin007@hotmail.com>
2026-04-29 01:26:59 +0200

    repeat password validation at register

--- frontend/src/components/organisms/forms/authentication/RegisterForm.vue
+++ frontend/src/components/organisms/forms/authentication/RegisterForm.vue
@@ -11,12 +11,17 @@
 
     const username = ref('')
     const password = ref('')
+    const repeatPassword = ref('')
     const currentSuccessAlert = ref(null)
     const currentErrorAlert = ref(null)
 
     async function handleRegister(e){
         try {
             e.preventDefault()
+
+            if (repeatPassword.value !== password.value) {
+                throw new Error("Repeated password is not equal to password");
+            }
 
             const response = await axios.post('/auth/register', {
                 username: username.value,
@@ -27,18 +32,23 @@
             currentSuccessAlert.value.displaySuccessMessage("Successfully registered a new account.")
         }
         catch (ex){
-            currentErrorAlert.value.displayErrorMessage(ex.response.data.message)
+            if (ex.response){
+                currentErrorAlert.value.displayErrorMessage(ex.response.data.message)
+            }
+            else {
+                currentErrorAlert.value.displayErrorMessage(ex.message)
+            }
         }
     }
 </script>
 
 <template>
     <form @submit="handleRegister">
-        <ErrorAlert ref="currentSuccessAlert" />
-        <SuccessAlert ref="currentErrorAlert" />
+        <ErrorAlert ref="currentErrorAlert" />
+        <SuccessAlert ref="currentSuccessAlert" />
         <BaseFormField labelName="Username" id="username" name="username" placeholder="Enter your username" v-model="username"/>
         <BaseFormField labelName="Password" type="password" id="password" name="password" placeholder="Enter your password" v-model="password"/>
-        <BaseFormField labelName="Repeat password" type="password" id="repeat_password" placeholder="Repeat your password"/>
+        <BaseFormField labelName="Repeat password" type="password" id="repeat_password" placeholder="Repeat your password" v-model="repeatPassword"/>
         <AuthsubmitBtn buttonText="Register" />
         <router-link to="/auth/login">Login into an existing account</router-link>
     </form>

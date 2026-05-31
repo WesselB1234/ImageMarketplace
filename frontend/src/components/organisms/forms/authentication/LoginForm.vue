9902e87a0789625b8fbcd9e89b8adac41e8268da
WesselB1234 <stosin007@hotmail.com>
2026-05-31 23:49:22 +0200

    Moving login based frontend code outside form component

--- frontend/src/components/organisms/forms/authentication/LoginForm.vue
+++ frontend/src/components/organisms/forms/authentication/LoginForm.vue
@@ -1,53 +1,19 @@
 <script setup>
-    import { ref } from 'vue'
-    import axios from '@/utils/axios.js'
-    import { useAuthStore } from '@/stores/authStore.js'
-    import { useErrorHandlingStore } from '@/stores/errorHandlingStore'
-    import router from '@/router'
-
     import AuthsubmitBtn from '@/components/atoms/buttons/forms/AuthsubmitBtn.vue'
     import BaseFormField from '@/components/molecules/forms/BaseFormField.vue'
-    import ErrorAlert from '@/components/atoms/errorHandling/ErrorAlert.vue'
-    import SuccessAlert from '@/components/atoms/errorHandling/SuccessAlert.vue'
-    
-    const authStore = useAuthStore()
-    const errorHandlingStore = useErrorHandlingStore()
 
-    const username = ref('')
-    const password = ref('')
-
-    const errorAlertRef = ref(null)
-
-    async function handleLogin(e) {
-        try {
-            e.preventDefault()
-
-            const response = await axios.post('/users/login', {
-                'username': username.value,
-                'password': password.value
-            })
-
-            authStore.setAuthToken(response.data.jwt)
-            errorHandlingStore.successMessage = 'Successfully logged in.'
-            router.push('/')
+    const props = defineProps({
+        vModel: {
+            type: Object,
+            required: true
         }
-        catch (ex){
-            if (ex.response){
-                errorAlertRef.value.displayErrorMessage(ex.response.data.message)
-            }
-            else {
-                errorAlertRef.value.displayErrorMessage(ex.message)
-            }
-        }
-    }
+    });
 </script>
 
 <template>
-    <form @submit="handleLogin">
-        <ErrorAlert ref="errorAlertRef"/>
-        <SuccessAlert />
-        <BaseFormField labelName="Username" id="username" placeholder="Enter your username" v-model="username"/>
-        <BaseFormField labelName="Password" type="password" id="password" placeholder="Enter your password" v-model="password"/>
+    <form>
+        <BaseFormField labelName="Username" id="username" placeholder="Enter your username" v-model="vModel.username"/>
+        <BaseFormField labelName="Password" type="password" id="password" placeholder="Enter your password" v-model="vModel.password"/>
         <AuthsubmitBtn text="Login" />
   </form>
 </template>
